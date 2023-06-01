890453b97f638badd2d713c5ea8c32dd363c901e
mj23000 <markusjacobsen@hotmail.com>
2023-06-01 11:03:29 +0200

    Fix PriorityAntenna

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/SpaceModels/PriorityAntenna.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/SpaceModels/PriorityAntenna.java
@@ -5,10 +5,9 @@ import dk.dtu.compute.se.pisd.roborally.model.Heading;
 
 public class PriorityAntenna extends Space {
 
-    public final int number;
-
     public PriorityAntenna(Board board, int x, int y, Heading[] edges) {
         super(board, x, y, edges);
+
         //TODO Auto-generated constructor stub
     }
 
