cfd9fe1d079a2a7f56411e2023535a911090e4e3
mj23000 <markusjacobsen@hotmail.com>
2023-06-06 12:18:32 +0200

    Change robot graphic Change player colors to suit graphics

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -56,7 +56,7 @@ import java.util.Optional;
 public class AppController implements Observer {
 
     final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
-    final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
+    final private List<String> PLAYER_COLORS = Arrays.asList("red", "brown", "orange", "yellow", "blue", "green");
 
     final private RoboRally roboRally;
 
