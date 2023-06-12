4eff41393690388a00bf1c2ab0beeca431073600
mj23000 <markusjacobsen@hotmail.com>
2023-06-12 10:52:22 +0200

    Modify playerId to start from 0

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -525,7 +525,7 @@ public class AppController implements Observer {
         int no = result.get();
 
         for (int i = 0; i < no; i++) {
-            Player player = new Player(board, PLAYER_COLORS.get(i), "Player " + (i + 1), i + 1);
+            Player player = new Player(board, PLAYER_COLORS.get(i), "Player " + i, i);
             board.addPlayer(player);
             player.setSpace(board.getStartGear(i));
         }
